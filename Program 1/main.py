fa77e6386f3ba5042f4651d1d83ed144b559405d
Aking <www.aking016@gmail.com>
2024-06-06 20:06:44 +0330

    Excerise 1

--- Program 1/main.py
+++ Program 1/main.py
@@ -1,6 +1,6 @@
 # برنامه ای بنویسید که ابتدا عددی را خوانده سپس عدد n را می خواند و nامین بیت عدد خوانده شده را معکوس میکند. یعنی چانانچه این بیت 1 باشد 0 میشود و برعکس
 
-def bitFlip(number):
+def bit_flip(number):
   if(number == '0'):
     return 1
   elif(number == '1'):
@@ -11,13 +11,13 @@ def bitFlip(number):
 while True:
   num = int(input("Enter your number: "))
 
-  binaryNum = bin(num)[2:]
-  print(f"Binary: {binaryNum}")
+  binary_num = bin(num)[2:]
+  print(f"Binary: {binary_num}")
 
   n = int(input("Enter the index: "))
 
-  if n < 0 or n >= len(binaryNum):
+  if n < 0 or n >= len(binary_num):
           print("Invalid index, please enter a valid index.")
           continue
   
-  print(f"Flipped bit of {binaryNum[n]}: {bitFlip(binaryNum[n])}")
+  print(f"Flipped bit of {binary_num[n]}: {bit_flip(binary_num[n])}")
